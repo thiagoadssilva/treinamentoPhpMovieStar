62dfb8b2eeb21b938f8b1b7e9c853bed4f416d25
Jose Da Silva <tjoses@minsait.com>
2024-06-11 04:57:04 -0300

    Finalização da funcionalidade de upload de imagem

--- models/User.php
+++ models/User.php
@@ -20,6 +20,10 @@ class User{
     public function generatePassword($password){
         return password_hash($password, PASSWORD_DEFAULT);
     }
+
+    public function imageGenerateName(){
+        return bin2hex(random_bytes(60))."jpg";
+    }
 }
 
 interface UserDAOInterface{
--- user_process.php
+++ user_process.php
@@ -8,6 +8,7 @@ require_once("dao/UserDAO.php");
 
 $message = new Message($BASE_URL);
 $userDao = new UserDAO($conn, $BASE_URL);
+$user = new User();
 
 //A função filter_input() no PHP é uma ferramenta poderosa para obter e filtrar dados de entrada provenientes de diferentes fontes, como formulários HTML, cookies, variáveis de ambiente e muito mais. 
 $type = filter_input(INPUT_POST, "type");
@@ -24,6 +25,30 @@ if ($type === "update") {
     $userData->lastname = $lastname;
     $userData->email = $email;
     $userData->bio = $bio;
+
+    // Trabalhando com imagem:
+    if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])){
+
+        $image = $_FILES["image"];
+        $imageTypes = ["image/jpg", "image/jpeg", "image/png",];
+        $jpgArray = ["image/jpg", "image/jpeg"];
+
+        if(in_array($image["type"], $imageTypes)){
+
+            if(in_array($image["type"], $jpgArray)){
+                $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+            }else {
+                $imageFile = imagecreatefrompng($image["tmp_name"]);
+            }
+
+            $imageName = $user->imageGenerateName();
+            imagejpeg($imageFile, "./img/users/".$imageName, 100);
+            $userData->image = $imageName;
+        }else {
+            $message->setMessage("Tipo de imagem inválida, por favor insira apenas (Jpg, Jpeg e Png)", "error", "back");
+        }
+    }
+
 
     $userDao->update($userData);
 } else if ($type === "changepassword") {
